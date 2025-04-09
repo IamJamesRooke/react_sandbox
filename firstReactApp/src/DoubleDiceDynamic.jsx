7469c5ccbb4e8090714f7d9b8f078d314911e945
IamJamesRooke <me@jamesrooke.blog>
2025-04-09 10:41:29 -0500

    dynamic double dice complete

--- firstReactApp/src/DoubleDiceDynamic.jsx
+++ firstReactApp/src/DoubleDiceDynamic.jsx
@@ -1,13 +1,18 @@
 export default function DoubleDiceDynamic() {
     const num1 = Math.floor(Math.random() * 3) + 1;
     const num2 = Math.floor(Math.random() * 3) + 1;
+    const isWinner = num1 == num2;
+    
+    const styles = {
+        color: isWinner ? "#00FF00" : "#FF4500", 
+        fontSize: "40px"
+    };
     
     return (
-        <>
-            {/* {num1 === num2 ? <h2>You Win 😀</h2> : <h2>You Lose 😭</h2>}    */}
-            {num1 === num2 && <h2>You Win 😀</h2>}
-            <p><strong>Num1:</strong> {num1}</p>
-            <p><strong>Num2:</strong> {num2}</p>
-        </>
+        <div className="DoubleDice" style = {styles}>
+            {num1 === num2 ? <h2>You Win 😀</h2> : <h2>You Lose 😭</h2>}   
+            <p><strong>Die 1:</strong> {num1}</p>
+            <p><strong>Die 2:</strong> {num2}</p>
+        </div>
     );
 }
