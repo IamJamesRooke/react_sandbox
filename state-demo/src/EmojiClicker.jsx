630562a137cd8cafa6dbeafb22ec5feae9f7bb37
IamJamesRooke <me@jamesrooke.blog>
2025-04-10 12:17:21 -0500

    face clicker done

--- state-demo/src/EmojiClicker.jsx
+++ state-demo/src/EmojiClicker.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
-import { useState } from 'react'
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+function randomEmoji() {
+    const faceEmojis = [
+        "😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇", "🙂", "🙃", "😉", "😌", "😍", "🥰", "😘", "😗", "😙", "😚", 
+        "😋", "😛", "😜", "🤪", "😝", "🤑", "🤗", "🤭", "🤫", "🤔", "🤐", "🤨", "😐", "😑", "😶", "😏", "😒", "🙄", "😬", "🤥", 
+        "😌", "😔", "😪", "🤤", "😴", "😷", "🤒", "🤕", "🤢", "🤮", "🤧", "😵", "🤯", "🤠", "🥳", "😎", "🤓", "🧐", "😕", "😟", 
+        "🙁", "☹️", "😮", "😯", "😲", "😳", "🥺", "😦", "😧", "😨", "😰", "😥", "😢", "😭", "😱", "😖", "😣", "😞", "😓", "😩", 
+        "😫", "🥱", "😤", "😡", "😠", "🤬", "😈", "👿", "💀", "☠️", "💩", "🤡", "👹", "👺", "👻", "👽", "👾", "🤖"
+    ];
+
+    return faceEmojis[Math.floor(Math.random() * faceEmojis.length)];
+}
 
 export default function EmojiClicker() {
-  
-    const [emojis, setEmojis] = useState(['😍'])
+    
+    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: randomEmoji() }]);
+    
     const addEmoji = () => {
-        setEmojis(oldEmojis => [...emojis, "😀"]);
+        setEmojis((oldEmojis) => [...emojis, { id: uuid(), emoji: randomEmoji() }]);
+    }
+
+    const deleteEmoji = (id) => {
+
+        setEmojis(prevEmojis => {
+            return prevEmojis.filter(e => e.id !== id)
+        })
     }
   
     return (
     <div>
-        
-        {emojis.map(e => (
-            <span style={{ fontSize: '4rem' }}>{e}</span> 
-        ))}
 
         <button onClick={addEmoji}>Add Emoji</button>
+        <br></br>
+
+        {emojis.map(e => (
+            <span 
+                onClick={() => deleteEmoji(e.id)} 
+                key={e.id} 
+                style={{ fontSize: '4rem', cursor: 'pointer' }}
+            >
+                {e.emoji}
+            </span> 
+        ))}
+
     </div>
   )
 }
