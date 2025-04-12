36518fd94ea70b3e8dc56d27d1aa325b108e6ed9
IamJamesRooke <me@jamesrooke.blog>
2025-04-12 12:04:49 -0500

    just copied the todo project, not relevant to me now

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,14 +1,19 @@
-import CssBaseline from '@mui/material/CssBaseline'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+import TodoList from "./TodoList";
+import Navbar from "./Navbar";
 
 function App() {
-
   return (
     <>
-      <CssBaseline/>
-      <h1>Todos</h1>
+      <CssBaseline />
+      <Navbar />
+      <TodoList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
